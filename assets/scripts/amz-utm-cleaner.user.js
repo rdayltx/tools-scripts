5975c17924ad960d198fdc59bf9ab192df02bdaf
DayLight <rdayltx@gmail.com>
2025-02-27 06:50:57 -0300

    add Amz UTM Cleaner user script to remove UTM parameters from Amazon URLs

---
+++ assets/scripts/amz-utm-cleaner.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name          Amz UTM Cleaner
+// @namespace     Pobre's Toolbox
+// @version       1.0
+// @icon          https://raw.githubusercontent.com/rdayltx/tools-scripts/main/assets/pobre_tools.ico
+// @description   Ferramentas do analista
+// @author        DayLight
+//
+// @match         *://*amazon.com.br/*
+// @exclude-match *://*amazon.com.br/hz/*
+// @exclude-match *://*amazon.com.br/progress-tracker/*
+// @exclude-match *://*amazon.com.br/kindle-dbs*
+// @exclude-match *://*amazon.com.br/mn*
+// @exclude-match *://*amazon.com.br/myk*
+// @exclude-match *://*amazon.com.br/b*
+// @exclude-match *://*amazon.com.br/gp/*
+// @exclude-match *://*amazon.com.br/s*
+// @exclude-match *://*amazon.com.br/prime*
+// @exclude-match *://*amazon.com.br/gcx*
+// @exclude-match *://*amazon.com.br/baby-reg*
+//
+// @run-at        document-start
+//
+//
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Pega a URL atual
+  let url = window.location.href;
+
+  // Procura o primeiro "#" ou "?" na URL e remove tudo após ele
+  let cleanedUrl = url.split("#")[0].split("?")[0];
+
+  // Se a URL foi alterada, redireciona para a URL limpa
+  if (url !== cleanedUrl) {
+    window.location.replace(cleanedUrl);
+  }
+})();
